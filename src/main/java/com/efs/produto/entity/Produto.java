315513b03a2687a87ee2777732af6d7b545aa43e
Maria Eduarda Scomparin <dudascomparin@gmail.com>
2026-03-12 21:54:04 -0300

    entity contrutor

--- src/main/java/com/efs/produto/entity/Produto.java
+++ src/main/java/com/efs/produto/entity/Produto.java
@@ -1,47 +1,49 @@
 package com.efs.produto.entity;
 
+import com.efs.produto.enums.StatusProduto;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produtos")
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private Double preco;
+
     private Integer quantidadeEmEstoque;
 
-    public Produto(Long id, String nome, Double preco, Integer quantidadeEmEstoque) {
-        this.id = id;
+    @Enumerated(EnumType.STRING)
+    private StatusProduto status;
+
+    public Produto() {}
+
+    public Produto(String nome, Double preco, Integer quantidadeEmEstoque, StatusProduto status) {
         this.nome = nome;
         this.preco = preco;
         this.quantidadeEmEstoque = quantidadeEmEstoque;
+        this.status = status;
     }
 
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public String getNome() {
-        return nome;
-    }
+    public Double getPreco() { return preco; }
+    public void setPreco(Double preco) { this.preco = preco; }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
-
-    public Integer getQuantidadeEmEstoque() {
-        return quantidadeEmEstoque;
-    }
-
+    public Integer getQuantidadeEmEstoque() { return quantidadeEmEstoque; }
     public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
         this.quantidadeEmEstoque = quantidadeEmEstoque;
     }
+
+    public StatusProduto getStatus() { return status; }
+    public void setStatus(StatusProduto status) { this.status = status; }
 }
---
+++ src/main/java/com/efs/produto/enums/StatusProduto.java
@@ -0,0 +1,12 @@
+package com.efs.produto.enums;
+
+public enum StatusProduto {
+    DISPONIVEL,
+    SEM_ESTOQUE,
+    DESCONTINUADO
+}
+
+
+
+
+
