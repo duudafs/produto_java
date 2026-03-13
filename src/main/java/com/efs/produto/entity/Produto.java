b45fe553920a48a9c7ba202cc63c5084f62fee5e
Maria Eduarda Scomparin <dudascomparin@gmail.com>
2026-03-12 21:47:21 -0300

    produto entity

---
+++ src/main/java/com/efs/produto/entity/Produto.java
@@ -0,0 +1,47 @@
+package com.efs.produto.entity;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private Double preco;
+    private Integer quantidadeEmEstoque;
+
+    public Produto(Long id, String nome, Double preco, Integer quantidadeEmEstoque) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setQuantidadeEmEstoque(Integer quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+}
